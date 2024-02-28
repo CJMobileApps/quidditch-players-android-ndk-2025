a1a9ea7dab7e6d2002e32c36507c028e4aca389a
Chris Jones <chris@cjmobileapps.com>
2024-02-27 23:16:30 -0600

    Update shimmer animations

--- app/src/main/java/com/cjmobileapps/quidditchplayersandroid/ui/util/ShimmerBrush.kt
+++ app/src/main/java/com/cjmobileapps/quidditchplayersandroid/ui/util/ShimmerBrush.kt
@@ -9,30 +9,38 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.cjmobileapps.quidditchplayersandroid.R
 
 @Composable
 fun shimmerBrush(showShimmer: Boolean = true): Brush {
-    val targetValue = 1000f
+    val widthOfShadowBrush = 500
+    val angleOfAxisY = 270f
+    val durationMillis = 1000f
+
     return if (showShimmer) {
         val shimmerColors = listOf(
-            Color.LightGray.copy(alpha = 0.6f),
-            Color.LightGray.copy(alpha = 0.2f),
-            Color.LightGray.copy(alpha = 0.6f),
+            Color.LightGray.copy(alpha = 0.3f),
+            Color.LightGray.copy(alpha = 0.5f),
+            Color.LightGray.copy(alpha = 1.0f),
+            Color.LightGray.copy(alpha = 0.5f),
+            Color.LightGray.copy(alpha = 0.3f),
         )
 
-        val transition = rememberInfiniteTransition(label = "todo add label")
+        val transition =
+            rememberInfiniteTransition(label = stringResource(R.string.animation_transition))
         val translateAnimation = transition.animateFloat(
             initialValue = 0f,
-            targetValue = targetValue,
+            targetValue = durationMillis,
             animationSpec = infiniteRepeatable(
-                animation = tween(700), repeatMode = RepeatMode.Reverse
+                animation = tween(durationMillis.toInt()), repeatMode = RepeatMode.Reverse
             ),
-            label = "todo add label"
+            label = stringResource(R.string.translateanimation)
         )
         Brush.linearGradient(
             colors = shimmerColors,
-            start = Offset.Zero,
-            end = Offset(x = translateAnimation.value, y = translateAnimation.value)
+            start = Offset(x = translateAnimation.value - widthOfShadowBrush, y = 0.0f),
+            end = Offset(x = translateAnimation.value, y = angleOfAxisY),
         )
     } else {
         Brush.linearGradient(
