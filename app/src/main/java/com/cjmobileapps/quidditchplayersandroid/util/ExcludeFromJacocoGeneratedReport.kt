ac1102167277410146b8606cdc9167ae98e5a465
Chris Jones <chris@cjmobileapps.com>
2024-04-22 02:14:29 -0500

    Add @ExcludeFromJacocoGeneratedReport

--- app/src/main/java/com/cjmobileapps/quidditchplayersandroid/util/ExcludeFromJacocoGeneratedReport.kt
+++ app/src/main/java/com/cjmobileapps/quidditchplayersandroid/util/ExcludeFromJacocoGeneratedReport.kt
@@ -1,5 +1,21 @@
 package com.cjmobileapps.quidditchplayersandroid.util
 
+
 @Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.FUNCTION)
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.ANNOTATION_CLASS,
+    AnnotationTarget.TYPE_PARAMETER,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.FIELD,
+    AnnotationTarget.LOCAL_VARIABLE,
+    AnnotationTarget.VALUE_PARAMETER,
+    AnnotationTarget.CONSTRUCTOR,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER,
+    AnnotationTarget.TYPE,
+    AnnotationTarget.FILE,
+    AnnotationTarget.TYPEALIAS,
+)
 internal annotation class ExcludeFromJacocoGeneratedReport
