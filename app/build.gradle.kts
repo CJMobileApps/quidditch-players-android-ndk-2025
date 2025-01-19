21bf585c37b6d9890fcd7df45a4e379170205df4
Chris Jones <chris@cjmobileapps.com>
2025-01-19 05:35:25 -0600

    Update build gradle some more

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("dagger.hilt.android.plugin")
-    id("de.mannodermaus.android-junit5")
-    id("com.google.devtools.ksp")
-    id("org.jlleitschuh.gradle.ktlint")
-    id("jacoco")
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.junit5)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.ktlint)
     alias(libs.plugins.compose.compiler)
+    id("jacoco")
+    id("dagger.hilt.android.plugin")
 }
 
 jacoco {
